39c88f2f78235e4498fdb7a7e79cf60632950117
Shane Finlayson <shane_finlayson@yahoo.com>
2019-02-16 14:45:51 -0700

    checkpoint-5-library: completed

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Album extends React.Component {
     render() {
         return (
-            <h1>This will be the Album</h1>
+            <section className='album'>
+                {this.props.match.params.slug} Album here
+            </section>
         )
     }
 }
--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import albumData from './../../data/albums.js';
 
 class Library extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {albums: albumData};
+    }
     render() {
         return (
-            <h1>This will be the Library</h1>
+            <section className='library'>
+                {this.state.albums.map( (album, index) => {
+                    return (
+                        <NavLink to={`/album/${album.slug}`}  key={index}>
+                            <img src={album.albumCover} alt={album.title}></img>
+                            <div>{album.title}</div>
+                            <div>{album.artist}</div>
+                            <div>{album.songs.length}</div>
+                        </NavLink>
+                    )
+                })}
+            </section>
         )
     }
 }
